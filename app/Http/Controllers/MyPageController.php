1f53616fad8c4924abe015760b52954ac6b47489
Nakamura Iori <nakamura@2ndwave.jp>
2022-07-27 15:58:29 +0900

    ユーザー情報の編集 ・一覧の名前がリンクになっているので押すと各ユーザーの情報と過去投稿が見れる

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -52,6 +52,52 @@ class MyPageController extends Controller
             }
         }
         $blogs = $query->sortable()->paginate(5);
-        return view('mypages.index', compact('blogs', 'user_name', 'from', 'until', 'keyword'));
+        return view('mypages.index', compact('blogs', 'user_name', 'from', 'until', 'keyword', 'user'));
+    }
+
+    public function show($id, Request $request)
+    {
+        $user = User::find($id);
+        $keyword = $request->input('keyword');
+        $from = $request->input('from');
+        $until = $request->input('until');
+        $user_id = $user->id;
+        $query = Blog::query();
+        if(!empty($keyword)) {
+            $query->join('users', function ($join) use ($user_id) {
+                $join->on('blogs.created_user_id', '=', 'users.id');
+            })
+            ->select('blogs.*', 'users.name')
+            ->where (function ($query) use ($user_id) {
+                $query->Where('users.id', 'LIKE', "%{$user_id}%");
+            });
+            $query->where (function ($query) use ($keyword) {
+                $query->Where('blogs.id', 'LIKE', "%{$keyword}%")
+                    ->orwhere('blogs.title', 'LIKE', "%{$keyword}%")
+                    ->orWhere('blogs.content', 'LIKE', "%{$keyword}%");
+            });
+            if(!empty($from) && !empty($until)) {
+                $query->where (function ($query) use ($from, $until) {
+                    $query->WhereBetween('blogs.created_at', [$from, $until]);
+                });
+            }
+        } else {
+            $query->join('users', function ($join) use ($user_id) {
+                $join->on('blogs.created_user_id', '=', 'users.id');
+            })
+            ->select('blogs.*', 'users.name')
+            ->where (function ($query) use ($user_id) {
+                $query->Where('users.id', 'LIKE', "%{$user_id}%");
+            });
+            if(!empty($from) && !empty($until)) {
+                $query->where (function ($query) use ($from, $until) {
+                    $query->WhereBetween('blogs.created_at', [$from, $until]);
+                });
+            }
+        }
+        //dump($user);
+        //die;
+        $blogs = $query->sortable()->paginate(5);
+        return view('mypages.show', compact('user', 'blogs', 'from', 'until', 'keyword'));
     }
 }
