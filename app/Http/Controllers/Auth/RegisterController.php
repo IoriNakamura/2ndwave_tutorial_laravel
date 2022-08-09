6f12d39fc3c8b1a30ac0e58681370b4691ce7ebd
Nakamura Iori <nakamura@2ndwave.jp>
2022-08-09 12:27:17 +0900

    パスワード編集の際に確認用パスワードの追加 パスワードと確認用が一致しない場合は、エラーメッセージが表示される。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,6 +49,8 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        dump($data);
+        die;
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
@@ -64,6 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
